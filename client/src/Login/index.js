f6c125b1fb4442d213dcc6b5c279e65185a29842
ilham377 <ilham377@gmail.com>
2023-07-12 15:35:59 +0700

    -

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -1,13 +1,11 @@
 import Login from "./Login";
 import Register from "./Register";
 import Form from "./Form";
-import Test_Login from "./Test_Login";
-import Test_Register from "./Test_Register";
+
 
 export {
     Login,
     Register,
     Form,
-    Test_Login,
-    Test_Register
+
 }
